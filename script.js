5764dfff20ef57d68c4433bc4ee1917a7657303b
shiraishi26 <ashiraishi@dl-techx-xedge.net>
2026-01-20 14:39:52 +0900

    カスタムドメイン変更

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ createApp({
       };
 
       // 実際のAPI URLに置き換えてください
-      const response = await axios.post('https://m3aya-test-acavnet.yellowsmoke-17d1c7de.japaneast.azurecontainerapps.io/api/INSERT', param);
+      const response = await axios.post('https://m3aya-testback.m3harbor.net/api/INSERT', param);
       console.log(response.data);
     };
 
     const readData = async () => {
       // 実際のAPI URLに置き換えてください
-      const response = await axios.get('https://m3aya-test-acavnet.yellowsmoke-17d1c7de.japaneast.azurecontainerapps.io/api/SELECT');
+      const response = await axios.get('https://m3aya-testback.m3harbor.net/api/SELECT');
       console.log(response.data);
       dataList.value = response.data.List;
     };
